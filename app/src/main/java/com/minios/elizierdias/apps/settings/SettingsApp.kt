f1035c1ed43b58625d5c861ee0ca980a1488a5d7
Kratos778 <elizierdias83@gmail.com>
2026-09-05 12:34:52 +0100

    fix: correct Modifier import path in SettingsApp

--- app/src/main/java/com/minios/elizierdias/apps/settings/SettingsApp.kt
+++ app/src/main/java/com/minios/elizierdias/apps/settings/SettingsApp.kt
@@ -37,7 +37,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier.Modifier
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
